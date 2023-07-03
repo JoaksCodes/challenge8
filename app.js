c240487431e5438cd0e674763202e50b9c3d3926
JoaksCodes <125510235+JoaksCodes@users.noreply.github.com>
2023-07-03 16:43:04 -0300

    Update app.js
    Agregamos pista al tercer intento.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-let respuesta = prompt("¿Por qué la luna es mayor que el sol?");
+let respuesta = prompt("Por qué la luna es mayor que el sol?");
+let intentos = 1;
 
-while (respuesta !== "Porque la dejan salir de noche!!!") {
+while (respuesta !== "Porque la dejan salir de noche") {
+  if (intentos === 3) {
+    alert("La gente grande puede...");
+  }
+
   respuesta = prompt("¿Por qué la luna es mayor que el sol?");
+  intentos++;
 }
 
-alert("Claro que si, pa!");
+alert("Claro que si pa!");
