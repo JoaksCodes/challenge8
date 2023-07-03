8746976a8fbe975f7e65519cbda7d365c10715f8
JoaksCodes <125510235+JoaksCodes@users.noreply.github.com>
2023-07-03 16:05:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let respuesta = prompt("¿Por qué la luna es mayor que el sol?");
+
+while (respuesta !== "Porque la dejan salir de noche!!!") {
+  respuesta = prompt("¿Por qué la luna es mayor que el sol?");
+}
+
+alert("Claro que si, pa!");
